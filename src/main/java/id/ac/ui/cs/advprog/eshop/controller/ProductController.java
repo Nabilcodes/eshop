ab4fe4d0255ae6ad6977f56026a6d1ffa1362168
Nabilcodes <nabil.muhammad@ui.ac.id>
2024-02-17 10:04:55 +0700

    modified naming to resolve deploy issues

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -17,7 +17,7 @@ public class ProductController {
     public String createProductPage(Model model) {
         Product product = new Product();
         model.addAttribute("product",product);
-        return "createProduct";
+        return "CreateProduct";
     }
 
     @PostMapping("/create")
@@ -30,7 +30,7 @@ public class ProductController {
     public String editProductPage(@RequestParam String productName, Model model) {
         Product product = service.findByNameAndDelete(productName);
         model.addAttribute("product", product);
-        return "editProduct";
+        return "EditProduct";
     }
 
     @PostMapping("/edit")
@@ -43,7 +43,7 @@ public class ProductController {
     public String productListPage(Model model){
         List<Product> allProducts = service.findAll();
         model.addAttribute("products",allProducts);
-        return "productlist";
+        return "Productlist";
     }
 
     @GetMapping("/delete")
